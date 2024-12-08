abb2f814ea2bf3f6df55ec02d87fa2cad10d5576
Manan Bhasin <mananbhasin5657@gmail.com>
2024-12-09 00:44:27 +0530

    manan final backend commit

--- Controller/course_Controller.js
+++ Controller/course_Controller.js
@@ -1,5 +1,6 @@
 const { query } = require("express");
 const Courses = require("../Models/courses");
+const Class = require("../Models/Classroom");
 const User = require("../Models/user");
 module.exports.fetchCourses = async (req, res) => {
   try {
@@ -65,5 +66,48 @@ module.exports.dashCourses = async (req, res) => {
     res.json(course);
   } catch (err) {
     res.json(err);
+  }
+};
+
+//!
+
+module.exports.getTeacherDashboardData = async (req, res) => {
+  try {
+    const { teacherId } = req.body; // Get teacher ID from the request body
+
+    // Fetch classes for the teacher
+    const classes = await Class.find({ teacher: teacherId })
+      .populate("course")
+      .populate("teacher");
+
+    // Fetch courses owned by the teacher
+    const courses = await Courses.find({ owner: teacherId }).populate(
+      "students"
+    );
+
+    // Calculate statistics
+    const totalStudents = new Set(); // To avoid counting duplicate students
+    const totalClasses = classes.length;
+
+    courses.forEach((course) => {
+      course.students.forEach((student) =>
+        totalStudents.add(student.toString())
+      );
+    });
+
+    const totalCourses = courses.length;
+    const totalUniqueStudents = totalStudents.size;
+
+    // Response data structure
+    res.json({
+      totalStudents: totalUniqueStudents,
+      totalCourses,
+      totalClasses,
+      classData: classes,
+      courseData: courses,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error", error });
   }
 };
